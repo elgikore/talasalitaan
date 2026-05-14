9871d0ea33422c6facdc472ef1cc1b56baa7df74
elgikore <elgienbertc@gmail.com>
2026-05-14 17:22:00 +1200

    can now enable/disable training and added another sample sentence

--- tokenizer.py
+++ tokenizer.py
@@ -1,12 +1,15 @@
 from talasalitaan import Talasalitaan
 import json
 
+TRAIN_MODE = False
 
 with open("settings.json", 'r', encoding="utf-8") as f:
     FOLDER_PATH = json.load(f)
 
 tokenizer = Talasalitaan()
-tokenizer.train(FOLDER_PATH["path/to/corpus"])
+
+if TRAIN_MODE:
+    tokenizer.train(FOLDER_PATH["path/to/corpus"])
 
 sp = tokenizer.spm_instance
 
@@ -20,6 +23,7 @@ texts = [
     "Ito ay pagsasalaysay ng mga katutubo sa kanilang paniniwalang lakas ng pisikal na kapaligiran at lakas ng pananampalataya ng lumilimbag sa kanilang buhay at kapalaran.",
     "Ang unang paglalayag na pambuong mundo sa ngalan ng Espanya ay nasundan ng apat pang mga ekspedisyon mula 1525 hanggang 1542. Sa ikaapat na panggagalugad, narating ni Ruy Lopez de Villalobos ang Kapuluan ng Pilipinas at pinangalanan niya ang mga pulo mula kay Philip II na noon ay may katayuan bilang tagapagmana ng trono ng Kaharian ng Espanya, bagaman hindi pa pormal na naitatag ang Pilipinas bilang opisyal na teritoryo ng Espanya.",
     "Sapagkat ang pagkilala sa katutubong karangalan at sa pantay at di-maikakait na mga karapatan ng lahat ng nabibilang sa angkan ng tao ay siyang saligan ng kalayaan, katarungan at kapayapaan sa daigdig."
+    "Ang Wikipedia ay isang proyektong online na ensiklopedya na panlahat, nakasulat sa maraming wika, at pinagtutulungan ang paggawa ng mga artikulo sa prinsipyong wiki. Naglalayon ang proyektong ito na mag-alok ng mga nilalaman na malayang muling magagamit, walang pinapanigan, at napapatunayan, na maaring baguhin at mapabuti ninuman. Nakikilala ang Wikipedia sa pamamagitan ng mga naitatag na prinsipyo. Nakalisensiya ang nilalaman nito sa ilalim ng Creative Commons BY-SA. Maari itong kopyahin at muling gamitin sa ilalim ng parehong lisensiya, na sumasailalim sa paggalang sa mga kondisyon. Ibinbigay ng Wikipedia ang mga nilalaman nito ng walang bayad, walang patalastas, at hindi nagsasamantala sa paggamit ng personal na datos ng mga gumagamit nito."
 ]
 
 for text in texts:
