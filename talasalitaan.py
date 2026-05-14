7466f2ee2d30fd007e168fa579b57b224544e840
elgikore <elgienbertc@gmail.com>
2026-05-14 16:46:27 +1200

    finished the wrapper

--- talasalitaan.py
+++ talasalitaan.py
@@ -1,5 +1,5 @@
 import sentencepiece as spm
-import subprocess
+import subprocess, tempfile
 
 NAME = "talasalitaan"
 
@@ -9,12 +9,23 @@ class Tasalitaan:
     def __init__(self):
         self.__MODEL_PREFIX = NAME
         self.__MODEL_FILE = NAME + ".model"
+        self.spm_instance = spm.SentencePieceProcessor(model_file=self.__MODEL_FILE)
     
     def train(self, corpus_folder: str) -> None:
         result = subprocess.run(
-                    f"cat \"{corpus_folder}\"/*.txt",
-                    shell=True,
-                    check=True,
-                    stdout=subprocess.PIPE,
-                    text=True
-                )
+            f"cat \"{corpus_folder}\"/*.txt",
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+    
+        with tempfile.NamedTemporaryFile(mode='w', delete=True, suffix=".txt") as temp_file:
+            temp_file.write(result.stdout)
+            
+            spm.SentencePieceTrainer.train(
+                input=temp_file.name,      
+                model_prefix=self.__MODEL_PREFIX,
+                vocab_size=16384,      
+                model_type="bpe"
+            )
