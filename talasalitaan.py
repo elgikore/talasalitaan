07e32809c323c281335b06783bc09f8459255eec
elgikore <elgienbertc@gmail.com>
2026-05-14 16:49:26 +1200

    docs

--- talasalitaan.py
+++ talasalitaan.py
@@ -7,11 +7,26 @@ class Tasalitaan:
     """Minimal adapter for SentencePiece, built for Filipino tokenization."""
     
     def __init__(self):
+        """
+        Initializes the wrapper. Auto-detects your .model and .vocab file only if it is on
+        the same folder as the script.
+        
+        You can access the instance by `var_name.spm_instance` and use the same functionalities
+        as SentencePiece.
+        """
         self.__MODEL_PREFIX = NAME
         self.__MODEL_FILE = NAME + ".model"
         self.spm_instance = spm.SentencePieceProcessor(model_file=self.__MODEL_FILE)
     
     def train(self, corpus_folder: str) -> None:
+        """
+        Trains the model of your specified corpus.
+
+        Parameters
+        ----------
+        corpus_folder : str
+            Corpus folder (not .txt file)
+        """
         result = subprocess.run(
             f"cat \"{corpus_folder}\"/*.txt",
             shell=True,
